bf5a3a6427172b4c9d6291e775b0231ea5454801
Cryptoleek13 <mjackson10396@icloud.com>
2023-03-17 14:14:02 -0400

    .

---
+++ Swift_Essentials.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+// variables and constants
+var greeting = "Hello, playground"
+
+let immutableItem = "I cannot be changed only reassigned"
+
+let yankeePlayer = "Aaron Judge"
+
+let quote = "He slugged a \"Tape Measure\" Home run"
+
+let summary = """
+The Yankees are strong favorites to win the 2023 world series
+"""
+
+print(yankeePlayer.count)
+print(quote.hasPrefix("He"))
+
+// Arrays
+var score: Double = 0
+
+let player: String = "Malik"
+let luckyNumber: Int = 13
+let pi: Double = 3.141
+var isEnabled: Bool = true
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Bluest Eye"])
