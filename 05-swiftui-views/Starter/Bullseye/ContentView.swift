a9e63f4a0f355cd006867e1e98010919cef34dba
Cryptoleek13 <mjackson10396@icloud.com>
2023-03-23 12:51:44 -0400

    Created first ios app

--- 05-swiftui-views/Starter/Bullseye/ContentView.swift
+++ 05-swiftui-views/Starter/Bullseye/ContentView.swift
@@ -2,14 +2,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Xcode!")
-        }
-        .padding()
-    }
+		VStack {
+			Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+			Text("89")
+			HStack {
+				Text("1")
+				Slider(value: .constant(50), in: 1.0...100.0)
+				Text("100")
+			}
+			Button("Hit me") {
+				/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+			}
+		}
+ }
 }
 
 struct ContentView_Previews: PreviewProvider {
