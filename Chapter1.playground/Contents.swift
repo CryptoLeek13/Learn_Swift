3570f444b9f7e514a0fd70702f33f29389b4e093
Cryptoleek13 <mjackson10396@icloud.com>
2023-03-29 19:32:24 -0400

    update to chapter one

--- Chapter1.playground/Contents.swift
+++ Chapter1.playground/Contents.swift
@@ -42,3 +42,7 @@ let averageRating: Double = (rating1 + rating2 + rating3) / 3
 /// Challenge 6: Electrical Power
 ///The power of an electrical appliance is calculated by multiplying the voltage by the current. Declare a constant named voltage of type Double and assign it a value. Then declare a constant called current of type Double and assign it a value. Finally, calculate the power of the electrical appliance you’ve just created, storing it in a constant called power of type Double.
 ///
+let voltage = 45.4
+let current = 67.8
+let power = voltage + current
+print(power)
