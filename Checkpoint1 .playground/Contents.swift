78fbc13f0cbf6bc3a3d53d4b0c11d12cd908628a
Cryptoleek13 <mjackson10396@icloud.com>
2023-03-30 18:31:54 -0400

    100 days of swift checkpoint 1 complete

---
+++ Checkpoint1 .playground/Contents.swift
@@ -0,0 +1,9 @@
+/// Create a constant holding any temperature in Celsius.
+///Convert it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+///Prints the result for the user, showing both the Celsius and Fahrenheit values.
+
+
+let celsisus = 6
+
+let fahrenheit = (celsisus * 9) / 5 + 32
+print(celsisus, fahrenheit)
